a308af6fa136e6869a9f0eb7d23e93125e0c128c
juliomaia28 <juliomaia2004@gmail.com>
2025-04-10 17:30:27 -0300

    subindo comentarios

--- notebooks/goldoddsdata.py
+++ notebooks/goldoddsdata.py
@@ -1,27 +1,29 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import avg, col, when
 spark = SparkSession.builder.appName("OddsGoldAnalytics").getOrCreate()
+
 silver_path = "s3a://dataodds/silver/cleaned"
 gold_path = "s3a://dataodds/gold/odds_aggregated"
 
-
-
-
 df = spark.read.format("delta").load(silver_path)
 
-# analisa no database se o time está jogando em casa ou fora
+
+
+# define se o time está jogando em casa ou fora
 df = df.withColumn(
-    "home_or_away",
-    when(col("team") == col("home_team"), "home").otherwise("away")
+    "local",
+    when(col("team") == col("home_team"), "casa").otherwise("fora")
 )
 
-# agrupa por time, casa de aposta e se esta jogando em casa/fora e calcula a média das odds para cada situaçao
-agg = df.groupBy("team", "market_type", "home_or_away").agg(
-    avg("odds").alias("avg_odds")
+# agrpa por time, casa de aposta e local (casa/fora), e calcula a média das odds
+agg = df.groupBy("team", "market_type", "local").agg(
+    avg("odds").alias("media_odds")
 )
 
-# Exibe os resultados
-print("database")
+# Renomeia as colunas 
+agg = agg.withColumnRenamed("team", "time") \
+         .withColumnRenamed("market_type", "casa_de_aposta")
+
+# mostra os dados e faz o carregamento na camada gold
 agg.show(30, truncate=False)
-
 agg.write.format("delta").mode("overwrite").option("mergeSchema", "true").save(gold_path)
